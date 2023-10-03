082f9850957f285ec78f9a930df7deb4322f7c91
Angel Alemany <angelemegeele@gmail.com>
2023-10-03 10:55:58 -0300

    feat: show dynamically user properties cards

--- components/property_card.php
+++ components/property_card.php
@@ -1,47 +1,73 @@
 <article class="property-card">
     <div class="card-header">
-        <h2 class="property-title">Casa a estrenar en la Zona del Barrio Villa Morra</h2>
-        <p class="property-code"><span class="semibold">ID:</span> BMG0001</p>
+        <h2 class="property-title"><?php echo $property["title"] ?></h2>
+        <p class="property-code"><span class="semibold">ID:</span> <?php echo $property["code_ref"] ?></p>
     </div>
     <div class="card-body">
-        <div class="property-thumbnail" style="background-image: url(<?php echo BASE_URL ?>/assets/img/property.jpg);">
-            <div class="play">
-                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
-                    <path fill="currentColor" d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
-                </svg>
-            </div>
+
+        <div class="property-thumbnail" style="background-image: url(<?php echo BASE_URL . $property["thumbnail"] ?>);">
+            <a href="<?php echo BASE_URL . "/tour/" . $property["slug"] ?>">
+                <div class="play">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+                        <path fill="currentColor" d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
+                    </svg>
+                </div>
+            </a>
         </div>
+
         <div class="property-details">
             <div class="details-container">
                 <div class="detail price">
                     <h3 class="detail-title">Precio</h3>
-                    <p class="detail-content">U$D 178.000</p>
+                    <p class="detail-content">U$D <?php echo $property["price"] ?></p>
                 </div>
                 <div class="detail rooms">
                     <h3 class="detail-title">Habitaciones</h3>
-                    <p class="detail-content">5</p>
+                    <p class="detail-content"><?php echo $property["rooms"] ?></p>
                 </div>
                 <div class="detail bathrooms">
                     <h3 class="detail-title">Baños</h3>
-                    <p class="detail-content">3</p>
+                    <p class="detail-content"><?php echo $property["bathrooms"] ?></p>
                 </div>
             </div>
             <div class="photos-container">
                 <h3 class="detail-title">Fotografías</h3>
-                <div class="photos-grid">
-                    <img class="property-photo" src="<?php echo BASE_URL ?>/assets/img/property.jpg" alt="Casa a estrenar" />
-                    <img class="property-photo" src="<?php echo BASE_URL ?>/assets/img/property.jpg" alt="Casa a estrenar" />
-                    <img class="property-photo" src="<?php echo BASE_URL ?>/assets/img/property.jpg" alt="Casa a estrenar" />
-                    <div class="photos-overflow">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-                            <path fill="currentColor" d="M0 96C0 60.7 28.7 32 64 32H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM323.8 202.5c-4.5-6.6-11.9-10.5-19.8-10.5s-15.4 3.9-19.8 10.5l-87 127.6L170.7 297c-4.6-5.7-11.5-9-18.7-9s-14.2 3.3-18.7 9l-64 80c-5.8 7.2-6.9 17.1-2.9 25.4s12.4 13.6 21.6 13.6h96 32H424c8.9 0 17.1-4.9 21.2-12.8s3.6-17.4-1.4-24.7l-120-176zM112 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z" />
-                        </svg>
-                        <span class="overflow-quantity">7+</span>
+                <a href="<?php echo BASE_URL . "/tour/" . $property["slug"] ?>">
+                    <div class="photos-grid">
+                        <?php
+                        // Verificar si existen imágenes en la propiedad
+                        if (isset($property['images']) && count($property['images']) > 0) {
+
+                            // Cantidad total de imágenes
+                            $totalImages = count($property['images']);
+                            $displayedImagesCount = 3;
+                            $overflowCount = $totalImages - $displayedImagesCount;
+
+                            // Muestra las primeras 3 imágenes (o menos si no hay 3)
+                            for ($i = 0; $i < min($displayedImagesCount, $totalImages); $i++) {
+                                echo '<img class="property-photo" src="' . BASE_URL . $property['images'][$i]['image_path'] . '" alt="' . $property["title"] . '">';
+                            }
+
+                            // Muestra el photos-overflow solo si hay más de 3 imágenes
+                            if ($overflowCount > 0) : ?>
+                                <div class="photos-overflow">
+
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                                        <path fill="currentColor" d="...[tu SVG aquí]...">
+                                    </svg>
+                                    <span class="overflow-quantity">+<?php echo $overflowCount; ?></span>
+
+                                </div>
+                        <?php endif;
+                        } else {
+                            echo "<p>Esta propiedad no tiene fotos...</p>";
+                        }
+                        ?>
                     </div>
-                </div>
+                </a>
             </div>
             <div class="see-more">
-                <a href="#">
+                <a href="<?php echo BASE_URL . "/tour/" . $property["slug"] ?>">
                     <span>Ver más</span>
                     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
                         <path fill="currentColor" d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
--- profile.php
+++ profile.php
@@ -5,19 +5,81 @@ require './admin/helpers/users.php';
 
 session_start();
 $username = $_GET["username"];
-
 $user = null;
+$properties = [];
 
-// preparar la consulta
-$stmt = $mydb->prepare("SELECT * FROM users WHERE username = ?");
+// Preparar la consulta con JOIN
+$query = "
+    SELECT 
+        u.*,
+        p.*, 
+        p.id AS property_id,
+        i.image_path,
+        i.id AS image_id,
+        i.width,
+        i.height
+    FROM users u
+    LEFT JOIN properties p ON u.id = p.user_id AND p.status = 'publicado'
+    LEFT JOIN images i ON p.id = i.property_id
+    WHERE u.username = ?
+";
+
+$stmt = $mydb->prepare($query);
 $stmt->bind_param("s", $username);
 
-// ejecutar la consulta
+// Ejecutar la consulta
 $stmt->execute();
 
 $result = $stmt->get_result();
+
 if ($result->num_rows > 0) {
-    $user = $result->fetch_assoc();
+    while ($row = $result->fetch_assoc()) {
+        if (!$user) {
+            // Extraer detalles del usuario de la primera fila
+            $user = [
+                'id' => $row['id'],
+                'username' => $row['username'],
+                'full_name' => $row['full_name'],
+                'company' => $row['company'],
+                'contact_email' => $row['contact_email'],
+                'whatsapp' => $row['whatsapp'],
+                'instagram' => $row['instagram'],
+                'facebook' => $row['facebook'],
+                'is_active' => $row['is_active'],
+                'role' => $row['role'],
+                'profile_picture' => $row['profile_picture'],
+                'properties' => []
+            ];
+        }
+
+        // Si hay una propiedad asociada y no está en el array $properties
+        if ($row['property_id'] && !isset($properties[$row['property_id']])) {
+            $properties[$row['property_id']] = [
+                'property_id' => $row['property_id'],
+                'title' => $row['title'],
+                'price' => $row['price'],
+                'rooms' => $row['rooms'],
+                'bathrooms' => $row['bathrooms'],
+                'code_ref' => $row['code_ref'],
+                'thumbnail' => $row['thumbnail'],
+                'slug' => $row['slug'],
+                'images' => []
+            ];
+        }
+
+        // Si hay una imagen asociada, añadirla al array de la propiedad correspondiente
+        if ($row['image_id']) {
+            $properties[$row['property_id']]['images'][] = [
+                'image_path' => $row['image_path'],
+                'image_id' => $row['image_id'],
+                'width' => $row['width'],
+                'height' => $row['height'],
+            ];
+        }
+    }
+
+    // Añadir las propiedades al usuario
+    $user['properties'] = array_values($properties);
 }
 
 $stmt->close();
@@ -70,9 +132,14 @@ $has_contact = $contact_email || $facebook || $instagram || $whatsapp;
             </div>
         </section>
         <section id="properties-listing" class="container px">
-            <?php include './components/property_card.php' ?>
-            <?php include './components/property_card.php' ?>
-            <?php include './components/property_card.php' ?>
+            <?php 
+            if (isset($user['properties']) && !empty($user['properties'])) {
+                foreach ($user['properties'] as $property) {
+                    include './components/property_card.php';
+                }
+            } else {
+                echo "<p>Este usuario no tiene propiedades publicadas...</p>";
+            }?>
         </section>
     </main>
 </body>
