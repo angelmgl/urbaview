c14d2692da2464f5c2d0d4b04049a231f45b0666
Angel Alemany <angelemegeele@gmail.com>
2023-10-08 20:33:38 -0300

    feat: add filters to users dashboard

--- admin/users.php
+++ admin/users.php
@@ -13,17 +13,53 @@ if ($_SESSION['role'] !== 'admin') {
     exit;
 }
 
-// recuperar registros de usuarios
-$query = "SELECT * FROM users";
-$result = $mydb->query($query);
+$full_name_value = isset($_GET['full_name']) ? htmlspecialchars($_GET['full_name']) : '';
+$role_value = isset($_GET['role']) ? $_GET['role'] : '';
+$is_active_value = isset($_GET['is_active']) ? $_GET['is_active'] : '';
+
+// Crear SQL dinámico
+$sql = "SELECT * FROM users WHERE 1=1";
+
+if ($full_name_value) {
+    $sql .= " AND full_name LIKE ?";
+    $full_name_value = "%$full_name_value%";
+}
+if ($role_value) {
+    $sql .= " AND role = ?";
+}
+if ($is_active_value !== '') {
+    $is_active_value = $is_active_value === 'true' ? 1 : 0;
+    $sql .= " AND is_active = ?";
+}
+
+$stmt = $mydb->prepare($sql);
+if ($full_name_value && $role_value && $is_active_value !== '') {
+    $stmt->bind_param('ssi', $full_name_value, $role_value, $is_active_value);
+} elseif ($full_name_value && $role_value) {
+    $stmt->bind_param('ss', $full_name_value, $role_value);
+} elseif ($full_name_value && $is_active_value !== '') {
+    $stmt->bind_param('si', $full_name_value, $is_active_value);
+} elseif ($role_value && $is_active_value !== '') {
+    $stmt->bind_param('si', $role_value, $is_active_value);
+} elseif ($full_name_value) {
+    $stmt->bind_param('s', $full_name_value);
+} elseif ($role_value) {
+    $stmt->bind_param('s', $role_value);
+} elseif ($is_active_value !== '') {
+    $stmt->bind_param('i', $is_active_value);
+}
+
+$stmt->execute();
+$result = $stmt->get_result();
 
 $users = [];
 if ($result->num_rows > 0) {
-    // Si hay resultados, recórrelos y añádelos al array $users.
     while ($row = $result->fetch_assoc()) {
         $users[] = $row;
     }
 }
+
+$full_name_value_display = str_replace('%', '', $full_name_value);
 
 // Cierra la conexión a la base de datos.
 $mydb->close();
@@ -55,13 +91,45 @@ $mydb->close();
         }
         ?>
 
+        <form method="GET" class="admin-form filters-container grid cols-4">
+            <!-- buscador por nombre -->
+            <div class="input-wrapper text-input">
+                <label for="full_name">Nombre completo: <span class="required">*</span></label>
+                <input type="text" id="full_name" name="full_name" value="<?php echo $full_name_value_display; ?>">
+            </div>
+            <!-- filtrar por rol -->
+            <div class="input-wrapper select-input">
+                <label for="role">Seleccionar rol:</label>
+                <select id="role" name="role">
+                    <option value="" <?php echo ($role_value == '') ? 'selected' : ''; ?>>Selecciona...</option>
+                    <option value="user" <?php echo ($role_value == 'user') ? 'selected' : ''; ?>>Usuario</option>
+                    <option value="admin" <?php echo ($role_value == 'admin') ? 'selected' : ''; ?>>Administrador</option>
+                </select>
+            </div>
+            <!-- filtrar por estado -->
+            <div class="input-wrapper select-input">
+                <label for="is_active">Seleccionar estado:</label>
+                <select id="is_active" name="is_active">
+                    <option value="" <?php echo ($is_active_value == '') ? 'selected' : ''; ?>>Selecciona...</option>
+                    <option value="true" <?php echo ($is_active_value == 'true') ? 'selected' : ''; ?>>Activo</option>
+                    <option value="false" <?php echo ($is_active_value == 'false') ? 'selected' : ''; ?>>Inactivo</option>
+                </select>
+            </div>
+            <!-- buscar -->
+            <button type="submit" class="btn btn-primary">Filtrar</button>
+        </form>
+
+        <?php if (empty($users)) { ?>
+            <p>No hay resultados para esta búsqueda...</p>
+        <?php } else { ?>
         <section class="users-grid">
             <?php
-            foreach ($users as $user) {
-                include './components/user_card.php';
-            }
+                foreach ($users as $user) {
+                    include './components/user_card.php';
+                }
             ?>
         </section>
+        <?php } ?>
     </main>
 </body>
 
