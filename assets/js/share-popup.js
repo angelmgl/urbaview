174c747461b826c156395d16ac2c58455355304e
Angel Alemany <angelemegeele@gmail.com>
2023-10-12 00:12:33 -0300

    feat: add copy link button

--- assets/js/share-popup.js
+++ assets/js/share-popup.js
@@ -9,3 +9,39 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     popup.classList.remove("open");
 });
+
+const copyBtn = document.querySelector(".copy");
+const copyIcon = document.getElementById("copy-icon");
+const checkIcon = document.getElementById("check-icon");
+
+copyBtn.addEventListener("click", function() {
+    // Obtener el valor del atributo data-url del botón
+    let urlToCopy = this.getAttribute('data-url');
+
+    // Usar el API de Clipboard para copiar el texto
+    navigator.clipboard.writeText(urlToCopy).then(() => {
+        console.log('Texto copiado al portapapeles');
+
+        // Ocultar el ícono de copia y mostrar el ícono de check
+        copyIcon.style.display = "none";
+        checkIcon.style.display = "inline";
+
+        // Cambiar el color de fondo del botón a verde claro
+        copyBtn.style.background = "#a8e6cf"; // Puedes ajustar este valor al verde claro que prefieras
+
+        // Llama a la función revertChanges luego de 3 segundos
+        setTimeout(revertChanges, 1000);
+
+    }).catch((err) => {
+        console.error('No se pudo copiar el texto: ', err);
+    });
+});
+
+function revertChanges() {
+    // Revertir los íconos a su estado original
+    copyIcon.style.display = "inline";
+    checkIcon.style.display = "none";
+
+    // Revertir el color de fondo del botón a su color original
+    copyBtn.style.background = "#e2efff";
+}
--- components/share_popup.php
+++ components/share_popup.php
@@ -1,12 +1,15 @@
 <!-- Load Facebook SDK for JavaScript -->
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) return;
-js = d.createElement(s); js.id = id;
-js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
-fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+<script>
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 
 <div id="share-popup">
     <div class="popup-content">
@@ -16,13 +19,20 @@ fjs.parentNode.insertBefore(js, fjs);
             </svg>
         </button>
         <h2>Compartir</h2>
-        <div class="fb-share-button" data-href="<?php echo $this_url ?>" data-layout="button_count"></div>
         <div class="social-options">
-            <button class="social-button facebook">
+            <button class="social-button copy" data-url="<?php echo $this_url ?>">
+                <svg id="copy-icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512">
+                    <path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
+                </svg>
+                <svg id="check-icon" style="display: none" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                    <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
+                </svg>
+            </button>
+            <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $this_url ?>" target="_BLANK" class="social-button facebook">
                 <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
                     <path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z" />
                 </svg>
-            </button>
+            </a>
             <a href="https://api.whatsapp.com/send?text=<?php echo $this_url ?>" target="_BLANK" class="social-button whatsapp">
                 <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
                     <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
